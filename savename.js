fe6094d5e9bd2a2d423c2751fa9d2cf50560dd05
DaSom253 <67749688+DaSom253@users.noreply.github.com>
2020-12-01 22:47:17 +0900

    restart project

---
+++ savename.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(".js-form"),
+input = form.querySelector("input");
+
+const USER_LS = "currentUser"
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+}
+
+function init() {}
+
+init();
